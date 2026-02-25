1cea9233a146e542e565155b8f60108e84ea7e9a
himanshu-shukla <hhimanshu030@gmail.com>
2026-02-25 12:50:53 +0530

    api

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { useDropzone } from "react-dropzone";
 import { useStore } from "@/store/useStore";
 import axios from "axios";
 
-const API_URL = "http://localhost:4000";
+const API_URL = "https://api.masterynexus.com";
 
 export default function Home() {
   const router = useRouter();
